09f72566711f9a2d94c4813a3988b1fc2b80ec56
ShauryaWadehra <34979006+ShauryaWadehra@users.noreply.github.com>
2022-07-25 02:07:14 +0530

    Time: 103 ms (90.44%), Space: 51.5 MB (50.99%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.cpp
+++ 217-contains-duplicate/217-contains-duplicate.cpp
@@ -1,11 +1,23 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-    sort(nums.begin(), nums.end());
-    for (int i=0; i<int(nums.size())-1; i++) {
-        if (nums[i]==nums[i+1])
-            return true;
+        ios_base::sync_with_stdio(0);
+        cin.tie(0);
+        cout.tie(0);
+        int size=nums.size();
+
+        unordered_map<int, int> contains;
+        
+        for(int i=0;i<size;i++)
+        {
+            contains[nums[i]]+=1;
+            if(contains[nums[i]]>1)
+            {
+                return true;
+            }
+                
+        }
+
+        return false;
     }
-    return false;    
-}
 };

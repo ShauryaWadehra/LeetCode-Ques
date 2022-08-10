718f2f0cf114c5d819cb758750e660069cff905c
ShauryaWadehra <34979006+ShauryaWadehra@users.noreply.github.com>
2022-08-10 19:32:49 +0530

    Time: 121 ms (91.54%), Space: 67.8 MB (10.92%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.cpp
+++ 53-maximum-subarray/53-maximum-subarray.cpp
@@ -4,10 +4,7 @@ public:
         int maxs = nums[0];
         int curr =0;
         for(int i :nums){
-            if(curr<0){
-                curr =0;
-            }
-            curr += i;
+            curr = max(i, curr+ i);
             maxs = max(maxs,curr);
         }
         return maxs;

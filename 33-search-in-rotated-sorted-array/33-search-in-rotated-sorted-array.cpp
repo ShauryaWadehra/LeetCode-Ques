1aa5e8a163e3cfd54deeb89ba9fcd619e99eae23
ShauryaWadehra <34979006+ShauryaWadehra@users.noreply.github.com>
2022-09-02 12:33:17 +0530

    Time: 3 ms (88.92%), Space: 11 MB (75.11%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
@@ -1,34 +1,22 @@
 class Solution {
 public:
     
-    int bin(vector<int>& nums, int low, int high, int target){
-        while(low<=high){
-            int mid = (low + high)/2;
-        if(nums[mid] == target){
-            return mid;
-        }else if(nums[mid] < target){
-            low = mid + 1;
-        }else{
-            high = mid - 1;
-            }
-        }
-        return -1;
-    }
     int search(vector<int>& nums, int target) {
-        if(nums.size() == 1){
-            if(nums[0] == target)return 0;
-            else return -1;
-        }
-        for(int i =1;i<nums.size();i++){
-            if(nums[i]>=nums[i-1]){
-                if(i == nums.size()-1){
-                    return bin(nums,0,nums.size()-1,target);
-                }
-                continue;
-            }else{
-                return max(bin(nums,0,i-1,target),bin(nums,i,nums.size()-1,target));
-            }
-        }
-        return -1;
+    int lo = 0, hi = nums.size();
+    while (lo < hi) {
+        int mid = (lo + hi) / 2;
+        
+        double num = (nums[mid] < nums[0]) == (target < nums[0])
+                   ? nums[mid]
+                   : target < nums[0] ? -INFINITY : INFINITY;
+                   
+        if (num < target)
+            lo = mid + 1;
+        else if (num > target)
+            hi = mid;
+        else
+            return mid;
     }
+    return -1;
+}
 };

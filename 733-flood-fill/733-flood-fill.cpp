b7752a3f05e199b0f50c1811ce08773ae142b368
ShauryaWadehra <34979006+ShauryaWadehra@users.noreply.github.com>
2023-06-08 11:58:03 +0530

    Update 733-flood-fill.cpp

--- 733-flood-fill/733-flood-fill.cpp
+++ 733-flood-fill/733-flood-fill.cpp
@@ -1,19 +1,26 @@
 class Solution {
-public:
-    void dfs(vector<vector<int>> &image, int i, int j, int val, int color){
-        if(i<0||i>=image.size()||j<0||j>=image[0].size()||image[i][j]==color||image[i][j]!=val){
-            return;
-        }
-        image[i][j] = color;
-        dfs(image,i-1,j,val,color);
-        dfs(image,i+1,j,val,color);
-        dfs(image,i,j-1,val,color);
-        dfs(image,i,j+1,val,color);
-        
+private:
+    void dfs(int r, int c, vector<vector<int>>& image, int nc, int ic){
+        int n = image.size();
+        int m = image[0].size();
+        image[r][c] = nc;
+        for(int drow = -1;drow<=1;drow++){
+                for(int dcol=-1;dcol<=1;dcol++){
+                    if((drow==-1&&dcol==-1) || (drow==-1&&dcol==1) || (drow==1&&dcol==-1) || (drow==1&&dcol==1)){
+                        continue;
+                    }
+                    int nrow = r+drow;
+                    int ncol = c+dcol;
+                    if(nrow<n && nrow >=0 && ncol<m && ncol >=0 && image[nrow][ncol] == ic && image[nrow][ncol] != nc){
+                        dfs(nrow,ncol,image,nc,ic);
+                    }                 
+                }
+            }
     }
+public:
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
-        int val = image[sr][sc];
-        dfs(image,sr,sc,val,color);
+        int ic = image[sr][sc];
+        dfs(sr,sc,image,color,ic);
         return image;
     }
 };

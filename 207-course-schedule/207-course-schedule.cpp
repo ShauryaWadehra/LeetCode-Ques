6ad55c4c80b12448171f2826eeb13b0f9332d20e
ShauryaWadehra <34979006+ShauryaWadehra@users.noreply.github.com>
2023-06-26 15:31:09 +0530

    Time: 25 ms (63.04%), Space: 13.8 MB (52.50%) - LeetHub

---
+++ 207-course-schedule/207-course-schedule.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<int> adj[numCourses];
+        for(auto i : prerequisites){
+            adj[i[0]].push_back(i[1]);
+        }
+        int inDeg[numCourses];
+        memset( inDeg, 0, numCourses*sizeof(int) );
+
+        for(int i = 0;i<numCourses;i++){
+            for(auto it : adj[i]){
+                inDeg[it]++;
+            }
+        }
+        queue<int> q;
+        for(int i = 0;i<numCourses;i++){
+            if(!inDeg[i]){
+                q.push(i);
+            }
+        }
+        vector<int> ans;
+        while(!q.empty()){
+            int n = q.front();
+            q.pop();
+            ans.push_back(n);
+            for(auto i : adj[n]){
+                inDeg[i]--;
+                if(!inDeg[i]){
+                    q.push(i);
+                }
+            }
+        }
+        if(ans.size()==numCourses){
+            return true;
+        }
+        return false;
+    }
+};

43f3c2118c7e6652f09c70e7301ffa45a7b922c5
ShauryaWadehra <34979006+ShauryaWadehra@users.noreply.github.com>
2023-08-31 23:04:02 +0530

    Time: 22 ms (56.83%), Space: 21.9 MB (28.68%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -2,26 +2,26 @@ class Solution
 {
 public:
     int findTargetSumWays(vector<int>& nums, int target) {
-    int totalSum = accumulate(nums.begin(), nums.end(), 0);
-    if (abs(target) > totalSum) {
-        return 0;
-    }
-
-    int n = nums.size();
-    vector<int> dp(2 * totalSum + 1, 0);
-    dp[totalSum] = 1; // Base case
-
-    for (int num : nums) {
-        vector<int> nextDp(2 * totalSum + 1, 0);
-        for (int i = 0; i < dp.size(); ++i) {
-            if (dp[i] > 0) {
-                nextDp[i + num] += dp[i];
-                nextDp[i - num] += dp[i];
-            }
+        int totalSum = accumulate(nums.begin(), nums.end(), 0);
+        if (abs(target) > totalSum) {
+            return 0;
         }
-        dp = nextDp;
-    }
 
-    return dp[totalSum + target];
-}
+        int n = nums.size();
+        vector<int> dp(2 * totalSum + 1, 0);
+        dp[totalSum] = 1; // Base case
+
+        for (int num : nums) {
+            vector<int> nextDp(2 * totalSum + 1, 0);
+            for (int i = 0; i < dp.size(); ++i) {
+                if (dp[i] > 0) {
+                    nextDp[i + num] += dp[i];
+                    nextDp[i - num] += dp[i];
+                }
+            }
+            dp = nextDp;
+        }
+
+        return dp[totalSum + target];
+    }
 };

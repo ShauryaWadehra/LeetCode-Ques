d2d2c0d4f7cca30dbed451b1d5575e3a9501d6de
ShauryaWadehra <34979006+ShauryaWadehra@users.noreply.github.com>
2023-09-02 19:23:47 +0530

    Time: 35 ms (80.41%), Space: 14.9 MB (93.58%) - LeetHub

--- 0329-longest-increasing-path-in-a-matrix/0329-longest-increasing-path-in-a-matrix.cpp
+++ 0329-longest-increasing-path-in-a-matrix/0329-longest-increasing-path-in-a-matrix.cpp
@@ -1,31 +1,25 @@
-class Solution {
+class Solution
+{
 public:
-    
-    int longestIncreasingPath(vector<vector<int>>& matrix) {
-        int rows = matrix.size();
-        if (!rows) return 0;
-        int cols = matrix[0].size();
-        
-        vector<vector<int>> dp(rows, vector<int>(cols, 0));
-        std::function<int(int, int)> dfs = [&] (int x, int y) {
-            if (dp[x][y]) return dp[x][y];
-            vector<vector<int>> dirs = {{-1, 0}, {1, 0}, {0, 1}, {0, -1}};
-            for (auto &dir : dirs) {
-                int xx = x + dir[0], yy = y + dir[1];
-                if (xx < 0 || xx >= rows || yy < 0 || yy >= cols) continue;
-                if (matrix[xx][yy] <= matrix[x][y]) continue;
-                dp[x][y] = std::max(dp[x][y], dfs(xx, yy));
-            }
-            return ++dp[x][y];
-        };
-        
-        int ret = 0;
-        for (int i = 0; i < rows; ++i) {
-            for (int j = 0; j < cols; ++j) {
-                ret = std::max(ret, dfs(i, j));
-            }
-        }
-        
-        return ret;
+    int dp[200][200]{}; // constraints are small enough that we can just set them to MAX
+    int maxPath, n, m;
+    int longestIncreasingPath(vector<vector<int>> &matrix)
+    {
+        maxPath = 0, n = size(matrix), m = size(matrix[0]);
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < m; j++)
+                maxPath = max(maxPath, solve(matrix, i, j, -1));
+        return maxPath;
+    }
+    int solve(vector<vector<int>> &mat, int i, int j, int prev)
+    {
+        if (i < 0 || j < 0 || i >= n || j >= m || mat[i][j] <= prev)
+            return 0;
+        if (dp[i][j])
+            return dp[i][j];
+        return dp[i][j] = 1 + max({solve(mat, i + 1, j, mat[i][j]),
+                                   solve(mat, i - 1, j, mat[i][j]),
+                                   solve(mat, i, j + 1, mat[i][j]),
+                                   solve(mat, i, j - 1, mat[i][j])});
     }
 };

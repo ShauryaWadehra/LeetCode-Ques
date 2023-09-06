be9e6428fb7538af80783a50b4eb21798d7e14c9
ShauryaWadehra <34979006+ShauryaWadehra@users.noreply.github.com>
2023-09-07 00:19:56 +0530

    Time: 1169 ms (41.45%), Space: 15.5 MB (57.26%) - LeetHub

---
+++ 0212-word-search-ii/0212-word-search-ii.cpp
@@ -0,0 +1,80 @@
+class TrieNode {
+public:
+    TrieNode* children[26];
+    bool isWord;
+    
+    TrieNode() {
+        for (int i = 0; i < 26; i++) {
+            children[i] = NULL;
+        }
+        isWord = false;
+    }
+};
+
+class Solution {
+public:
+    vector<string> findWords(vector<vector<char>>& board, vector<string>& words) {
+        for (int i = 0; i < words.size(); i++) {
+            insert(words[i]);
+        }
+        
+        int m = board.size();
+        int n = board[0].size();
+        
+        TrieNode* node = root;
+        vector<string> result;
+        
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                search(board, i, j, m, n, node, "", result);
+            }
+        }
+        
+        return result;
+    }
+private:
+    TrieNode* root = new TrieNode();
+    
+    void insert(string word) {
+        TrieNode* node = root;
+        int curr = 0;
+        
+        for (int i = 0; i < word.size(); i++) {
+            curr = word[i] - 'a';
+            if (node->children[curr] == NULL) {
+                node->children[curr] = new TrieNode();
+            }
+            node = node->children[curr];
+        }
+        
+        node->isWord = true;
+    }
+    
+    void search(vector<vector<char>>& board, int i, int j, int m, int n, TrieNode* node, string word, vector<string>& result) {
+        if (i < 0 || i >= m || j < 0 || j >= n || board[i][j] == '#') {
+            return;
+        }
+        
+        char c = board[i][j];
+        
+        node = node->children[c - 'a'];
+        if (node == NULL) {
+            return;
+        }
+        
+        word += board[i][j];
+        if (node->isWord) {
+            result.push_back(word);
+            node->isWord = false;
+        }
+        
+        board[i][j] = '#';
+        
+        search(board, i - 1, j, m, n, node, word, result);
+        search(board, i + 1, j, m, n, node, word, result);
+        search(board, i, j - 1, m, n, node, word, result);
+        search(board, i, j + 1, m, n, node, word, result);
+        
+        board[i][j] = c;
+    }
+};

5d2571b512b8ad2b293e90d109722553e61c515a
ShauryaWadehra <34979006+ShauryaWadehra@users.noreply.github.com>
2023-10-12 16:59:29 +0530

    Time: 224 ms (12.76%), Space: 102.7 MB (8.82%) - LeetHub

--- 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.cpp
+++ 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.cpp
@@ -10,7 +10,7 @@ public:
         }
         int preSum = 0;
         int cnt = 0;
-        map<int,int> mp;
+        unordered_map<int,int> mp;
         mp[0] = 1;
         for(int i = 0;i<nums.size();i++){
             preSum += nums[i];

58777827f5657c6f18571c28715cfaac7ba64945
monaco <34979006+ShauryaWadehra@users.noreply.github.com>
2024-10-28 18:16:53 -0500

    Time: 0 ms (100.00%), Space: 16.7 MB (12.87%) - LeetHub

--- 1046-last-stone-weight/1046-last-stone-weight.py
+++ 1046-last-stone-weight/1046-last-stone-weight.py
@@ -6,5 +6,5 @@ class Solution:
             temp1 = heapq.heappop(stones)
             temp2 = heapq.heappop(stones)
             if temp1 !=temp2:
-                heapq.heappush(stones,-abs(temp1-temp2))
-        return abs(stones[0]) if len(stones) else 0
+                heapq.heappush(stones,temp1-temp2)
+        return -stones[0] if len(stones) else 0
